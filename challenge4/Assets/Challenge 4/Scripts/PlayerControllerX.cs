b58bd634780169416e2d4fc7509f409968bd12ac
garywm <119637588+garywm@users.noreply.github.com>
2023-11-02 00:30:49 +0000

    completed Challenge 4
    Problems with particle effect playing

---
+++ challenge4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    private float speed = 500;
+    private GameObject focalPoint;
+
+    public bool hasPowerup;
+    public GameObject powerupIndicator;
+    public int powerUpDuration = 5;
+    public float boostTimer = 1.5f;
+    public bool boostAvailable;
+
+    private float normalStrength = 10; // how hard to hit enemy without powerup
+    private float powerupStrength = 25; // how hard to hit enemy with powerup
+
+    private float boost = 15.0f;
+    public ParticleSystem boostEffect;
+    
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+        boostAvailable = true;
+    }
+
+    void Update()
+    {
+        Movement();
+        // Set powerup indicator position to beneath player
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.6f, 0);  
+    }
+
+    // If Player collides with powerup, activate powerup
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Powerup"))
+        {
+            Destroy(other.gameObject);
+            hasPowerup = true;
+            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
+        }
+    }
+
+    //movement and turboBoost timer
+    private void Movement()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space) && boostAvailable)
+        {
+            playerRb.AddForce(focalPoint.transform.forward * boost, ForceMode.Impulse);
+            StartCoroutine(BoostCooldown());
+            boostEffect.Play();
+            boostAvailable = false;
+        } else
+        {
+            // Add force to player in direction of the focal point (and camera)
+            playerRb.AddForce(focalPoint.transform.forward * verticalInput * speed * Time.deltaTime);
+        }
+    }
+    // Coroutine to count down powerup duration
+    IEnumerator BoostCooldown()
+    {
+        yield return new WaitForSeconds(boostTimer);
+        boostAvailable = true;
+    }
+    // Coroutine to count down powerup duration
+    IEnumerator PowerupCooldown()
+    {
+        yield return new WaitForSeconds(powerUpDuration);
+        hasPowerup = false;
+        powerupIndicator.SetActive(false);
+    }
+
+    // If Player collides with enemy
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer =  other.gameObject.transform.position - transform.position; 
+           
+            if (hasPowerup) // if have powerup hit enemy with powerup force
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * powerupStrength, ForceMode.Impulse);
+            }
+            else // if no powerup, hit enemy with normal strength 
+            {
+                enemyRigidbody.AddForce(awayFromPlayer * normalStrength, ForceMode.Impulse);
+            }
+
+
+        }
+    }
+
+
+
+}
